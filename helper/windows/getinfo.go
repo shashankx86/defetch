9cad7b5de13a2c5d32b7d6b1a41c302f4fb6aaac
Shashank Shekhar Pandey <shashankspis20@gmail.com>
2024-07-29 15:58:45 +0530

    windows: info retrieval(2) (wip)

--- helper/windows/getinfo.go
+++ helper/windows/getinfo.go
@@ -15,6 +15,7 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+// GetWindowsInfo retrieves all system information on a Windows machine
 func GetWindowsInfo() helper.SysInfo {
 	// Hostname
 	hostname, _ := os.Hostname()
@@ -22,7 +23,7 @@ func GetWindowsInfo() helper.SysInfo {
 	// Current User
 	currentUser, _ := user.Current()
 
-	// Operating System Info
+	// OS Name, Version, Build Number
 	osName, osVersion, osBuildNumber := getOSInfo()
 
 	// Manufacturer and Model
@@ -48,6 +49,21 @@ func GetWindowsInfo() helper.SysInfo {
 	desktopEnvironment := "Windows Explorer"
 	windowManager := "DWM"
 	currentTheme := getCurrentTheme()
+
+	// CPU Information
+	cpuModel, cpuCores, cpuThreads, cpuSpeed := getCPUInfo()
+
+	// GPU Information
+	gpuModel, gpuDriverVersion := getGPUInfo()
+
+	// Memory Information
+	totalRAM, usedRAM, freeRAM := getMemoryInfo()
+
+	// Disk Information
+	totalDiskSpace, usedDiskSpace, freeDiskSpace := getDiskInfo()
+
+	// Battery Information
+	batteryPercentage, batteryStatus := getBatteryInfo()
 
 	return helper.SysInfo{
 		Hostname:    hostname,
@@ -77,6 +93,33 @@ func GetWindowsInfo() helper.SysInfo {
 		},
 		PackageManagement: helper.PackageManagementInfo{
 			PackageCount: installedPackages,
+		},
+		Hardware: helper.HardwareInfo{
+			CPU: helper.CPUInfo{
+				Model:      cpuModel,
+				Cores:      cpuCores,
+				Threads:    cpuThreads,
+				BaseSpeed:  cpuSpeed.Base,
+				BoostSpeed: cpuSpeed.Boost,
+			},
+			GPU: helper.GPUInfo{
+				Model:         gpuModel,
+				DriverVersion: gpuDriverVersion,
+			},
+			Memory: helper.MemoryInfo{
+				Total: totalRAM,
+				Used:  usedRAM,
+				Free:  freeRAM,
+			},
+			Disk: helper.DiskInfo{
+				Total: totalDiskSpace,
+				Used:  usedDiskSpace,
+				Free:  freeDiskSpace,
+			},
+			Battery: helper.BatteryInfo{
+				Percentage: batteryPercentage,
+				Status:     batteryStatus,
+			},
 		},
 	}
 }
@@ -194,4 +237,94 @@ func getCurrentTheme() string {
 		return "Dark Mode"
 	}
 	return "Light Mode"
+}
+
+// Get CPU Information
+func getCPUInfo() (model string, cores, threads int, speed struct{ Base, Boost string }) {
+	var cpuInfo struct {
+		Name          string
+		NumberOfCores uint32
+		ThreadCount   uint32
+		MaxClockSpeed uint32
+	}
+	err := wmi.Query("SELECT Name, NumberOfCores, ThreadCount, MaxClockSpeed FROM Win32_Processor", &cpuInfo)
+	if err != nil {
+		return "Unknown", 0, 0, struct{ Base, Boost string }{"Unknown", "Unknown"}
+	}
+	model = cpuInfo.Name
+	cores = int(cpuInfo.NumberOfCores)
+	threads = int(cpuInfo.ThreadCount)
+	speed = struct{ Base, Boost string }{fmt.Sprintf("%d MHz", cpuInfo.MaxClockSpeed), "Unknown"} // Boost speed not available
+	return
+}
+
+// Get GPU Information
+func getGPUInfo() (model, driverVersion string) {
+	var gpuInfo struct {
+		Name          string
+		DriverVersion string
+	}
+	err := wmi.Query("SELECT Name, DriverVersion FROM Win32_VideoController", &gpuInfo)
+	if err != nil {
+		return "Unknown", "Unknown"
+	}
+	model = gpuInfo.Name
+	driverVersion = gpuInfo.DriverVersion
+	return
+}
+
+// Get Memory Information
+func getMemoryInfo() (total, used, free uint64) {
+	var memStatus windows.MEMORYSTATUSEX
+	memStatus.Length = uint32(unsafe.Sizeof(memStatus))
+	err := windows.GlobalMemoryStatusEx(&memStatus)
+	if err != nil {
+		return 0, 0, 0
+	}
+	total = memStatus.TotalPhys
+	used = total - memStatus.AvailPhys
+	free = memStatus.AvailPhys
+	return
+}
+
+// Get Disk Information
+func getDiskInfo() (total, used, free uint64) {
+	var freeBytesAvailable, totalNumberOfBytes, totalNumberOfFreeBytes uint64
+	err := windows.GetDiskFreeSpaceEx(nil, &freeBytesAvailable, &totalNumberOfBytes, &totalNumberOfFreeBytes)
+	if err != nil {
+		return 0, 0, 0
+	}
+	total = totalNumberOfBytes
+	used = totalNumberOfBytes - totalNumberOfFreeBytes
+	free = totalNumberOfFreeBytes
+	return
+}
+
+// Get Battery Information
+func getBatteryInfo() (percentage int, status string) {
+	var batteryInfo struct {
+		DesignCapacity     int
+		FullChargeCapacity int
+		Status             int
+	}
+	err := wmi.Query("SELECT DesignCapacity, FullChargeCapacity, BatteryStatus FROM Win32_Battery", &batteryInfo)
+	if err != nil {
+		return 0, "Unknown"
+	}
+	if batteryInfo.FullChargeCapacity > 0 {
+		percentage = (batteryInfo.DesignCapacity * 100) / batteryInfo.FullChargeCapacity
+	} else {
+		percentage = 0
+	}
+	switch batteryInfo.Status {
+	case 1:
+		status = "Discharging"
+	case 2:
+		status = "Charging"
+	case 3:
+		status = "Fully Charged"
+	default:
+		status = "Unknown"
+	}
+	return
 }
